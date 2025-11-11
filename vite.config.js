129d097346ca3bc9ab99f0cd09bd7d933263348f
bogdanov <bogdanov@it-limon.ru>
2025-11-11 10:56:53 +0500

    Подключение vike только в build-сборке

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,17 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import vike from 'vike/plugin';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()/*, vike()*/],
+export default defineConfig(({ command, mode }) => {
+  // vike подключаем только для сборки в продакшн.
+  // В dev-сборке шрифты загружаются несколько раз
+  // Воможно, из-за этого: https://github.com/vikejs/vike/issues/261
+  if (command === 'serve' && mode === 'development') {
+    return {
+      plugins: [react()],
+    };
+  } else {
+    return {
+      plugins: [react(), vike()],
+    };
+  }
 });
