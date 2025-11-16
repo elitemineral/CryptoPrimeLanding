4497b2c4e818b81e0597b115d12c6a2261b6dce0
elitemineral <dimonbek@mail.ru>
2025-11-17 01:33:47 +0500

    Добавил react-router

--- main.jsx
+++ main.jsx
@@ -1,9 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './src/app/App.jsx';
+import { BrowserRouter } from 'react-router';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 );
--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,5 +1,6 @@
 import { Header } from '../header/Header';
 import { Services } from '../services';
+import { Route, Routes } from 'react-router';
 
 import style from './app.module.scss';
 import '../shared/styles/styles.scss';
@@ -8,9 +9,16 @@ import '../shared/i18n';
 
 export function App() {
   return (
-    <div className={style.wrapper}>
-      <Header />
-      <Services />
-    </div>
+    <Routes>
+      <Route
+        path='/'
+        element={
+          <div className={style.wrapper}>
+            <Header />
+            <Services />
+          </div>
+        }
+      />
+    </Routes>
   );
 }
