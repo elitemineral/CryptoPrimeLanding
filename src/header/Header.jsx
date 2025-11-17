56dff5994f21fd5a749e2aa91da8c61ea8c3236e
elitemineral <dimonbek@mail.ru>
2025-11-18 01:24:13 +0500

    Досброс

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -1,6 +1,6 @@
 import { useTranslation } from 'react-i18next';
 import style from './header.module.scss';
-import { LanguagePicker } from '../shared/ui';
+// import { LanguagePicker } from '../shared/ui';
 import { useCallback } from 'react';
 
 export function Header() {
@@ -18,7 +18,7 @@ export function Header() {
 
   return (
     <header className={style.header}>
-      <LanguagePicker onChange={hanldeChangeLanguage} value={language} />
+      {/* <LanguagePicker onChange={hanldeChangeLanguage} value={language} /> */}
       <h1 className={style.heading}>CryptoPrime</h1>
       <p className={style.subheading}>{t('header.subheading')}</p>
     </header>
--- vite.config.js
+++ vite.config.js
@@ -17,20 +17,20 @@ export default defineConfig(({ command, mode }) => {
   //   };
   return {
     plugins: [react(), visualizer(),],
-    // build: {
-    //   rollupOptions: {
-    //     output: {
-    //       manualChunks(id) {
-    //         if (id.includes('node_modules')) {
-    //           return id
-    //             .toString()
-    //             .split('node_modules/')[1]
-    //             .split('/')[0]
-    //             .toString();
-    //         }
-    //       },
-    //     },
-    //   },
-    // },
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              return id
+                .toString()
+                .split('node_modules/')[1]
+                .split('/')[0]
+                .toString();
+            }
+          },
+        },
+      },
+    },
   };
 });
